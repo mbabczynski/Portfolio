b252180b2f0efb47a4224f590ef058557b132329
mbabczynski <babczynski@o2.pl>
2023-07-23 12:07:30 +0200

    footer test updated

--- Playwright/test_3_click_logo_in_footer.py
+++ Playwright/test_3_click_logo_in_footer.py
@@ -6,7 +6,7 @@ from playwright.sync_api import Page, expect
 def test_click_logo_in_footer(page: Page) -> None:
     page.goto("/")
     page.locator("app-footer").get_by_role("link",
-    name="fizjoterapia medycyna manualna szczecin Marika Kopaczyk").click()
+    name="fizjoterapia medycyna manualna szczecin").click()
     sleep(2)
     expect(page).to_have_url("/home")
 
