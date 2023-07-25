4e3c915bbf3e8b5554a84f0ac0343df321b55f0d
mbabczynski <babczynski@o2.pl>
2023-07-25 13:38:46 +0200

    Playwright tests updated

--- Playwright/test_2_navigation_bar.py
+++ Playwright/test_2_navigation_bar.py
@@ -1,71 +1,46 @@
-
 from playwright.sync_api import Page, expect
 
-# Tests asserting URL, title and focus on proper button after clicking on each link in top navigation bar
+
+# Tests asserting URL, title and focus on proper button after clicking on each link in top navigation bar.
 
 
-def test_top_navigation_bar_home(page: Page):
+def locators(page: Page):       # list of dictionaries of links in top navigation bar containing button(locator) ,
+                                # url, and title of each link.
+    top_navigation_bar = [{'button': page.locator("app-nav").get_by_role("link", name="Strona główna"),
+                           'url': "/",
+                           'title': "Fizjoterapia i rehabilitacja w Szczecinie – centrum FizjoNature"},
+                          {'button': page.locator("app-nav").get_by_role("link", name="Zespół"),
+                           'url': "/zespol",
+                           'title': "Poznaj zespół centrum  fizjoterapii i rehabilitacji w Szczecinie"},
+                          {'button': page.locator("app-nav").get_by_role("link", name="Oferta"),
+                           'url': "/oferta",
+                           'title': "Zabiegi manualne – zabiegi fizjoterapii i rehabilitacji"},
+                          {'button': page.locator("app-nav").get_by_role("link", name="Cennik"),
+                           'url': "/cennik",
+                           'title': "Cennik centrum medycyny manualnej w Szczecinie – ceny zabiegów"},
+                          {'button': page.locator("app-nav").get_by_role("link", name="Blog"),
+                           'url': "/blog",
+                           'title': "Blog centrum fizjoterapii i rehabilitacji w Szczecinie"},
+                          {'button': page.locator("app-nav").get_by_role("link", name="FAQ"),
+                           'url': "/faq",
+                           'title': "FAQ centrum medycyny manualnej – najczęstsze pytania pacjentów"},
+                          {'button': page.locator("app-nav").get_by_role("link", name="Kontakt"),
+                           'url': "/kontakt",
+                           'title': "Kontakt z centrum medycyny manualnej FizjoNature w Szczecinie"}
+                          ]
+    return top_navigation_bar
+
+
+def test_top_navigation_bar(page: Page):
     page.goto("/")
-    page.locator("app-nav").get_by_role("link", name="Strona główna").click()
-    expect(page).to_have_url("/")
-    expect(page).to_have_title("Fizjoterapia i rehabilitacja w Szczecinie – centrum FizjoNature")
-    expect(page.locator("app-nav").get_by_role("link", name="Strona główna")).to_be_focused()
-    expect(page.locator("app-nav").get_by_role("link", name="Zespół")).not_to_be_focused()
-
-
-def test_top_navigation_bar_zespol(page: Page):
-    page.goto("/")
-    page.locator("app-nav").get_by_role("link", name="Zespół").click()
-    expect(page).to_have_url("/zespol")
-    expect(page).to_have_title("Poznaj zespół centrum  fizjoterapii i rehabilitacji w Szczecinie")
-    expect(page.locator("app-nav").get_by_role("link", name="Zespół")).to_be_focused()
-    expect(page.locator("app-nav").get_by_role("link", name="Strona główna")).not_to_be_focused()
-
-
-def test_top_navigation_bar_oferta(page: Page):
-    page.goto("/")
-    page.locator("app-nav").get_by_role("link", name="Oferta").click()
-    expect(page).to_have_url("/oferta")
-    expect(page).to_have_title("Zabiegi manualne – zabiegi fizjoterapii i rehabilitacji")
-    expect(page.locator("app-nav").get_by_role("link", name="Oferta")).to_be_focused()
-    expect(page.locator("app-nav").get_by_role("link", name="Strona główna")).not_to_be_focused()
-
-
-def test_top_navigation_bar_cennik(page: Page):
-    page.goto("/")
-    page.locator("app-nav").get_by_role("link", name="Cennik").click()
-    expect(page).to_have_url("/cennik")
-    expect(page).to_have_title("Cennik centrum medycyny manualnej w Szczecinie – ceny zabiegów")
-    expect(page.locator("app-nav").get_by_role("link", name="Cennik")).to_be_focused()
-    expect(page.locator("app-nav").get_by_role("link", name="Strona główna")).not_to_be_focused()
-
-
-def test_top_navigation_bar_blog(page: Page):
-    page.goto("/")
-    page.locator("app-nav").get_by_role("link", name="Blog").click()
-    expect(page).to_have_url("/blog")
-    expect(page).to_have_title("Blog centrum fizjoterapii i rehabilitacji w Szczecinie")
-    expect(page.locator("app-nav").get_by_role("link", name="Blog")).to_be_focused()
-    expect(page.locator("app-nav").get_by_role("link", name="Strona główna")).not_to_be_focused()
-
-
-def test_top_navigation_bar_faq(page: Page):
-    page.goto("/")
-    page.locator("app-nav").get_by_role("link", name="FAQ").click()
-    expect(page).to_have_url("/faq")
-    expect(page).to_have_title("FAQ centrum medycyny manualnej – najczęstsze pytania pacjentów")
-    expect(page.locator("app-nav").get_by_role("link", name="FAQ")).to_be_focused()
-    expect(page.locator("app-nav").get_by_role("link", name="Strona główna")).not_to_be_focused()
-
-
-def test_top_navigation_bar_kontakt(page: Page):
-    page.goto("/")
-    page.locator("app-nav").get_by_role("link", name="Kontakt").click()
-    expect(page).to_have_url("/kontakt")
-    expect(page).to_have_title("Kontakt z centrum medycyny manualnej FizjoNature w Szczecinie")
-    expect(page.locator("app-nav").get_by_role("link", name="Kontakt")).to_be_focused()
-    expect(page.locator("app-nav").get_by_role("link", name="Strona główna")).not_to_be_focused()
-
-
-
+    subpages = locators(page)
+    for subpage in subpages:
+        subpage['button'].click()
+        page.wait_for_load_state('load')
+        expect(page).to_have_url(subpage['url'])
+        expect(page).to_have_title(subpage['title'])
+        expect(subpage['button']).to_be_focused()
+        for i in subpages:
+            if i != subpage:
+                expect(i['button']).not_to_be_focused()
 
--- Playwright/test_3_click_logo_in_footer.py
+++ Playwright/test_3_click_logo_in_footer.py
@@ -1,13 +1,16 @@
 from time import sleep
 from playwright.sync_api import Page, expect
 
+
 # Test checks page url (homepage) after clicking logo in footer
 
+
 def test_click_logo_in_footer(page: Page) -> None:
-    page.goto("/")
+    page.goto("/zespol")
     page.locator("app-footer").get_by_role("link",
-    name="fizjoterapia medycyna manualna szczecin").click()
-    sleep(2)
-    expect(page).to_have_url("/home")
+                                           name="fizjoterapia medycyna manualna szczecin").click()
+    page.wait_for_load_state('load')
+    sleep(3)
+    expect(page).to_have_url("/")
 
 
